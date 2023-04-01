584fcaa5a2415b45d851aba9637c18218688f004
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-04-01 21:36:30 +0530

    Create Assignment_Balanced Parenthesis.java

---
+++ Assignment_Balanced Parenthesis.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+	public static boolean Blncd_Par(String str,Stack<Character> stck){
+		if(str.length()%2!=0) return false;
+		for(char chr:str.toCharArray()){
+			if(chr=='(' || chr=='[' || chr=='{') stck.push(chr);
+			else if(chr==')' && !stck.isEmpty() && stck.peek()=='(') stck.pop();
+			else if(chr==']' && !stck.isEmpty() && stck.peek()=='[') stck.pop();
+			else if(chr=='}' && !stck.isEmpty() && stck.peek()=='{') stck.pop();
+		}
+		return stck.isEmpty();
+	}
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		Stack<Character> stck=new Stack<>();
+		boolean val=Blncd_Par(str,stck);
+		if(val) System.out.println("Yes");
+		else System.out.println("No");
+		sc.close();		
+	}
+}
+
