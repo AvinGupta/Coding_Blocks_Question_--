d444b66603b5e88a6d2b49e5779719773983ef81
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-07-24 22:04:26 +0530

    Create Queens_Combinations.java

---
+++ _Codes_--/Queens_Combinations.java
@@ -0,0 +1,25 @@
+package Java_Backtracking;
+import java.util.Scanner;
+public class Queen_Combination {
+    public static void Combination(int[] arr,int no_Queen,int placed,String answer,int ind){
+        if(placed==no_Queen){
+            System.out.println(answer);
+            return;
+        }
+        for(int i=ind+1;i<arr.length;i++){
+            if(arr[i]==0){
+                arr[i]=1;
+                Combination(arr,no_Queen,placed+1,answer+"b"+i+"Q"+placed,i);
+                arr[i]=0;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int board_size=sc.nextInt();
+        int[] board=new int[board_size];
+        int no_Queen=sc.nextInt();
+        Combination(board,no_Queen,0,"",-1);
+        sc.close();
+    }
+}
