23f25e8da28fd701228dcabefb7d8f20aa08653b
Avin_gupta <100298456+AvinGupta@users.noreply.github.com>
2023-08-04 18:53:03 +0530

    Update Rat Chases its cheese.java

--- Assignment_5_--/Rat Chases its cheese.java
+++ Assignment_5_--/Rat Chases its cheese.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Main {
-    static boolean var=false;
+    static boolean var=false; 
     public static void Display_answer(int[][] arr){
         for(int i=0;i< arr.length;i++){
             for(int j=0;j<arr[i].length;j++){
